ddd3f3ac2790e05a5c4db28c274693db8d2d7236
nguyen duc luong phat <nguyenducluongphat7049@gmail.com>
2020-07-28 20:44:14 +0700

    update insertion sort

--- insertion_sort/index.js
+++ insertion_sort/index.js
@@ -2,12 +2,7 @@ const array = [13, 14, 15, 12, 11, 13, 5, 6];
 
 
 var b = (function inSertSort (array) {
-  const valueDecision = array[0];
-  var result = [];
-
-  
   function init() {
-    result[0] = valueDecision;
     Array.prototype.insert = function(index, value) {
       this.splice(index, 0, value);
     }
@@ -15,10 +10,11 @@ var b = (function inSertSort (array) {
   }
 
   function find() {
-    const valueDecision = array[0];
+    let valueDecision = array[0];
     var result = [valueDecision];
     for (let i = 1; i < array.length; i++) {
       const tempV = array[i];
+      valueDecision = result.slice(-1)[0];
       if (tempV < valueDecision) {
         let indexInsert = 0;
         for (let j = 0; j < result.length; j++) {
@@ -28,23 +24,36 @@ var b = (function inSertSort (array) {
         }
         result.insert(indexInsert, tempV);
       } else {
-        const indexStart = result.indexOf(valueDecision);
-        let indexInsert = indexStart;
-        for (let j = indexStart; j < result.length; j++) {
-          if (tempV > result[j]) {
-            indexInsert = j + 1;
-          }
-        }
-        result.insert(indexInsert, tempV);
+        result.insert(i, tempV);
       }
     }
     return result;
+  }
 
+  
+  function findByRecursive(curArray = [array[0]], curIndex = 1) {
+    if (curIndex >= array.length) {
+      return curArray;
+    }
+    const nextNumber = array[curIndex];
+    const lastNum = curArray.slice(-1)[0];
+
+    if (lastNum > nextNumber) {
+      let indexInsert = 0;
+      for (let j = 0; j < curArray.length - 1; j++) {
+        indexInsert = nextNumber > curArray[j] ? j + 1 : indexInsert;
+      }
+      curArray.insert(indexInsert, nextNumber);
+    } else {
+      curArray.insert(curIndex, nextNumber);
+    }
+    return findByRecursive(curArray, ++curIndex);
   }
 
   return {
     init,
     find,
+    findByRecursive
   }
 })(array);
 
